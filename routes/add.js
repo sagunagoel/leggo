0dd3abc52757c1775479ba86ccfe983f774738ef
vagrant <vagrant@precise32.(none)>
2014-02-14 06:59:31 +0000

    merged changes 1

--- routes/add.js
+++
@@ -1,14 +0,0 @@
-var data = require("../data.json");
-
-exports.addFriend = function(req, res) {    
-	// Your code goes here
-	var name=req.query.name;
-	var descrip=req.query.description;
-	var json =
-	{'name': name,
-	'description': descrip,
-	'imageURL': 'http://lorempixel.com/400/400/people'
-	}
-	data["friends"].push(json);
-	res.render('add',data);
- };
