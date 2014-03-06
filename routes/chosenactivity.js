cd72613a6c8c77661712696a61d8ccd9c0e3cc65
vagrant <vagrant@precise32.(none)>
2014-03-06 12:42:52 +0000

    fixed bug with details page

--- routes/chosenactivity.js
+++ routes/chosenactivity.js
@@ -1,6 +1,16 @@
 var data = require('../public/data.json');
 
+
 exports.view = function(req, res){
-	console.log(data);
-	res.render('chosenactivity',data);
+	// console.log(data);
+	var projectID=req.params.id;
+	console.log("URL id is " + projectID);
+	for(var i=0;i<data['activities'].length;i++){
+    	if (data['activities'][i]['id']==projectID) {
+    		datanew= data['activities'][i];
+    	}
+}
+	console.log(datanew);
+	res.render('chosenactivity',datanew);
+
 };
